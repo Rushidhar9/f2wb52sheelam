350c0d1958e72f15233872b4b8a828d321c14fee
Rushidhar9 <111991958+Rushidhar9@users.noreply.github.com>
2022-11-02 18:57:39 -0500

    added grid build pug js files

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var biryaniRouter = require('./routes/biryani');
+var gridRouter = require('./routes/gridbuild');
+
 
 var app = express();
 
@@ -22,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/biryani',biryaniRouter)
+app.use('/biryani',biryaniRouter);
+app.use('/gridbuild',gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
